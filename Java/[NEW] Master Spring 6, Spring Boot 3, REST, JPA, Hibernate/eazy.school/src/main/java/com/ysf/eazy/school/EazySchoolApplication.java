984557f3fac47febb6e90c1fd6fa6e69ab80a308
Muhammad Yousaf Khan <myousaf.khan@systemsltd.com>
2024-09-24 12:00:17 +0500

    add JPA config annotations for reference

--- Java/[NEW] Master Spring 6, Spring Boot 3, REST, JPA, Hibernate/eazy.school/src/main/java/com/ysf/eazy/school/EazySchoolApplication.java
+++ Java/[NEW] Master Spring 6, Spring Boot 3, REST, JPA, Hibernate/eazy.school/src/main/java/com/ysf/eazy/school/EazySchoolApplication.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+// Following 2 annotations are only required if JPA repositories
+// and entities are not placed under the main package that contains
+// the main method class
+//@EnableJpaRepositories("com.ysf.eazy.school")
+//@EntityScan("com.ysf.eazy.school")
 public class EazySchoolApplication {
 
 	public static void main(String[] args) {
